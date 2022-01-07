2f12036fd6bf10cdb6e33a6d634f1f1441a77be3
MunnaAhmed130 <munnaahmed2025@gmail.com>
2022-01-07 16:57:54 +0600

    commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import { HashLink } from 'react-router-hash-link';
+import { Link } from 'react-router-dom';
+// import { HashLink } from 'react-router-hash-link';
 import "./Header.css"
 
 const navStyle = {
@@ -8,25 +9,29 @@ const navStyle = {
 }
 const Header = () => {
     return (
-        <div>
             <Navbar collapseOnSelect expand="lg">
                 <Container>
                     <Navbar.Brand href=""></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav" >
-                        <Nav className="m-auto">
-                            <Nav.Link as={HashLink} style={navStyle} to='/home'>Home</Nav.Link>
-                            <Nav.Link as={HashLink} style={navStyle} to='/projects'>Projects</Nav.Link>
-                            <Nav.Link as={HashLink} style={navStyle} to="/blogs">Blogs</Nav.Link>
-                            <Nav.Link as={HashLink} style={navStyle} to='/contact'>Contact</Nav.Link>
-                            <Nav.Link as={HashLink} style={navStyle} to='/aboutMe'>About Me</Nav.Link>
-                            <Nav.Link href="https://drive.google.com/file/d/1RhVvtP6IrrtuftlTtwQZAvgmQVRizMcy/view?usp=sharing" target="_blank">Resume</Nav.Link>
+                    <Nav className="m-auto">
+
+                        {/* <Nav.Link to='/home'>Home</Nav.Link>
+                        <Nav.Link to='/projects'>Projects</Nav.Link>
+                        <Nav.Link to="/blogs">Blogs</Nav.Link>
+                        <Nav.Link to='/contact'>Contact</Nav.Link>
+                        <Nav.Link to='/aboutMe'>About Me</Nav.Link> */}
+                        <Link to='/home'>Home</Link>
+                        <Link to='/projects'>Projects</Link>
+                        <Link to="/blogs">Blogs</Link>
+                        <Link to='/contact'>Contact</Link>
+                        <Link to='/aboutMe'>About Me</Link>
+                        <a href="https://drive.google.com/file/d/1RhVvtP6IrrtuftlTtwQZAvgmQVRizMcy/view?usp=sharing" target="_blank" rel='noreferrer noopener'>Resume</a>
 
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
-            </Navbar>
-        </div>
+        </Navbar>
     );
 };
 
