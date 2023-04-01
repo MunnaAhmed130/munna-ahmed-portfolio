7bf9a2d5ce2560966f992830527cf0fb61518a50
Munna Ahmed <munnaahmed2025@gmail.com>
2023-04-01 21:35:16 +0600

    commit

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,26 +1,26 @@
-import { Button } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import { Row, Col } from 'react-bootstrap';
-import { useParams } from 'react-router';
-import SkeletonDetails from '../../Skeletons/SkeletonDetails';
-import Detail from '../Detail/Detail';
-import Footer from '../Footer/Footer';
-import './Details.css'
+import { Button } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Row, Col } from "react-bootstrap";
+import { useParams } from "react-router";
+import SkeletonDetails from "../../Skeletons/SkeletonDetails";
+import Detail from "../Detail/Detail";
+import Footer from "../Footer/Footer";
+import "./Details.css";
 
 const Details = () => {
     const { _id } = useParams();
     const [details, setDetails] = useState(null);
     // const [details, setDetails] = useState([]);
     useEffect(() => {
-        fetch(`https://munna-ahmed-porfolio-server.herokuapp.com/portfolio/${_id}`)
-            .then(res => res.json())
-            .then(data => setDetails(data))
-    }, [])
-    console.log(details)
+        fetch(`/fakedata.json`)
+            .then((res) => res.json())
+            .then((data) => setDetails(data));
+    }, []);
+    console.log(details);
     // const { title, project_demo_1, project_demo_2, project_demo_3, github_client_repo, github_server_repo, live_link, p_1, p_2, p_3 } = details;
 
     return (
-        <div >
+        <div>
             {/* <Row className="detail-row">
                 <Col lg={4} className="project-demo-col" >
                     <div className="project-demo-container"><img src={details.project_demo_1} className="project-demo " alt="" /></div></Col>
@@ -31,11 +31,11 @@ const Details = () => {
 
             </Row> */}
 
-            {details && <div className="details">
-
-                <Detail details={details} key={details._id}></Detail>
-
-            </div>}
+            {details && (
+                <div className="details">
+                    <Detail details={details} key={details._id}></Detail>
+                </div>
+            )}
             {!details && <SkeletonDetails></SkeletonDetails>}
             <Footer />
         </div>
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,37 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import { Row, Col, Spinner } from 'react-bootstrap';
-import SkeletonProjects from '../../Skeletons/SkeletonProjects';
-import Footer from '../Footer/Footer';
-import Project from '../Project/Project';
-import './Projects.css'
+import React, { useEffect, useState } from "react";
+import { Row, Col, Spinner } from "react-bootstrap";
+import SkeletonProjects from "../../Skeletons/SkeletonProjects";
+import Footer from "../Footer/Footer";
+import Project from "../Project/Project";
+import "./Projects.css";
 
 const Projects = () => {
     const [project, setProject] = useState(null);
     // const [success, setSuccess] = useState(false)
     useEffect(() => {
-        const Url = `https://munna-ahmed-porfolio-server.herokuapp.com/portfolio`;
+        const Url = `fakedata.json`;
         fetch(Url)
-            .then(res => res.json())
-            .then(data => setProject(data))
-    }, [])
-    console.log(project)
+            .then((res) => res.json())
+            .then((data) => setProject(data));
+    }, []);
+    console.log(project);
 
     return (
-        <div id="projects" >
+        <div id="projects">
             <div className="projects-container ">
-
                 <h2>Projects</h2>
                 {/* <SkeletonElement type="avatar" /> */}
                 {/* <Spinner animation="border" /> */}
-                {project && <Row className="projects">
-                    {
-                        project.map(project => <Project project={project} key={project._id}></Project>)
-                    }
-                </Row>}
+                {project && (
+                    <Row className="projects">
+                        {project.map((project) => (
+                            <Project
+                                project={project}
+                                key={project._id}
+                            ></Project>
+                        ))}
+                    </Row>
+                )}
                 {/* {
                     !project && <div className='spin' > <Spinner animation="border" /></div>
                 } */}
-                {!project && <Row xl={3} lg={2} md={1} sm={1} className="projects">{[1, 2, 3].map((n) => <SkeletonProjects key={n} />)}</Row>}
+                {!project && (
+                    <Row xl={3} lg={2} md={1} sm={1} className="projects">
+                        {[1, 2, 3].map((n) => (
+                            <SkeletonProjects key={n} />
+                        ))}
+                    </Row>
+                )}
             </div>
             <Footer />
         </div>
