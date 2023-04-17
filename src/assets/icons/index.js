91c34bbaaad48365a696c81565a5fa31a02c504a
Munna Ahmed <munnaahmed2025@gmail.com>
2023-04-17 22:57:53 +0600

    commit

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,5 +1,6 @@
 import javascript from "./javascript.svg";
 import css3 from "./css3.svg";
 import html5 from "./html5.svg";
+import tailwindCSS from "./tailwind-css.svg";
 
-export { javascript, css3, html5 };
+export { javascript, css3, html5, tailwindCSS };
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,9 +4,7 @@ import { profilePic } from "../assets";
 import { HiCheckBadge } from "react-icons/hi2";
 import { SiGmail, SiLinkedin } from "react-icons/si";
 import { BsGithub } from "react-icons/bs";
-import { FaLinkedin } from "react-icons/fa";
-import Ripple from "./button/Ripple";
-import { css3, html5, javascript } from "../assets/icons";
+import { css3, html5, javascript, tailwindCSS } from "../assets/icons";
 // to-[#77a1fd]  from-[#4b52e4]
 
 const Intro = () => {
@@ -15,6 +13,7 @@ const Intro = () => {
     { icon: javascript, title: "JavaScript" },
     { icon: css3, title: "CSS3" },
     { icon: html5, title: "HTML5" },
+    { icon: tailwindCSS, title: "Tailwind CSS" },
   ];
 
   return (
@@ -86,69 +85,73 @@ const Intro = () => {
           </a>
         </div>
       </div> */}
-      <div className="max-w-5xl w-full  mx-auto px-20">
-        <div className="flex items-center justify-start gap-5">
-          <div className="rounded-full  sm:w-28 sm:h-28 w-24 h-24 object-cover  flex items-center duration-[400ms] transition-all  border-[#ffffff]   p-1">
-            <span className="bg-gradient-to-t to-[#9b77fd]  from-[#455cc4]  w-full h-full inline-block rounded-full transition-all">
+      <div className="max-w-5xl w-full mx-auto sm:px-20 px-5">
+        <div className="">
+          <div className="flex items-center justify-start gap-5">
+            <div className="rounded-full object-cover  flex items-center duration-[400ms] transition-all  border-[#ffffff] ">
               <img
-                className="mx-auto w-full h-full object-cover rounded-full"
+                className="mx-auto bg-gradient-to-t to-[#9b77fd]  from-[#455cc4] sm:w-28 sm:h-28 w-24 h-24 object-cover rounded-full"
                 src={profilePic}
                 alt="Profile-photo"
               />
-            </span>
-          </div>
-          <div>
-            <div className=" sm:text-2xl text-xl flex items-center justify-start ">
-              <p className="text-white inline-block">Munna Ahmed</p>
-              &nbsp;
-              <HiCheckBadge className="text-blue-500" />
             </div>
-            <Typewriter
-              options={{ autoStart: true, loop: true, delay: 100 }}
-              onInit={(typewriter) => {
-                typewriter
-                  .typeString(
-                    "<span class='typewriter'>MERN Stack Developer<span/>"
-                  )
-                  .pauseFor(2000)
-                  .deleteAll()
-                  .typeString(
-                    "<span class='typewriter'>Front-End Developer<span/>"
-                  )
-                  .pauseFor(2000)
-                  .deleteAll()
-                  .typeString("<span class='typewriter'>React Developer<span/>")
-                  .pauseFor(2000)
-                  .deleteAll()
-                  .start();
-              }}
-            />
-            <div className=" flex items-start justify-start ">
-              {icons.map((Icon) => (
-                <button>
-                  <Icon className="text-white text-lg mr-3 mt-1" />
-                </button>
+            <div>
+              <div className=" sm:text-2xl xs:text-xl text-base flex items-center justify-start ">
+                <p className="text-white inline-block">Munna Ahmed</p>
+                &nbsp;
+                <HiCheckBadge className="text-blue-500" />
+              </div>
+              <Typewriter
+                options={{ autoStart: true, loop: true, delay: 100 }}
+                onInit={(typewriter) => {
+                  typewriter
+                    .typeString(
+                      "<span class='typewriter'>MERN Stack Developer<span/>"
+                    )
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .typeString(
+                      "<span class='typewriter'>Front-End Developer<span/>"
+                    )
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .typeString(
+                      "<span class='typewriter'>React Developer<span/>"
+                    )
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .start();
+                }}
+              />
+              <div className=" flex items-start justify-start ">
+                {icons.map((Icon) => (
+                  <button>
+                    <Icon className="text-white text-lg mr-3 mt-1" />
+                  </button>
+                ))}
+              </div>
+            </div>
+          </div>
+          <p className="text-white my-14 tracking-wider max-w-xl">
+            Hi, I'm a React Developer, self-taught with experience in web
+            development, designing and creating responsive and user-friendly
+            websites.
+          </p>
+          <div>
+            <p className="text-white text-xl  uppercase">Skills</p>
+            <div>
+              {skills.map((skill) => (
+                <div className=" inline-block mt-5 w-40" key={skill.title}>
+                  <div>
+                    <img
+                      src={skill.icon}
+                      className={`p-1 w-10 h-10 inline-block `}
+                    />
+                    <span className="text-white">{skill.title}</span>
+                  </div>
+                </div>
               ))}
             </div>
-          </div>
-        </div>
-        <p className="text-white my-14 tracking-wider max-w-xl">
-          Hi, I'm a React Developer, self-taught with experience in web
-          development, designing and creating responsive and user-friendly
-          websites.
-        </p>
-        <div>
-          <p className="text-white text-xl  uppercase">Skills</p>
-          <div>
-            {skills.map((skill) => (
-              <div
-                className="inline-block border mt-5 border-slate-500 border-collapse w-40"
-                key={skill.title}
-              >
-                <img src={skill.icon} className="w-10 h-10 inline-block p-1" />
-                <span className="text-white">{skill.title}</span>
-              </div>
-            ))}
           </div>
         </div>
       </div>
