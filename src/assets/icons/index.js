546f1409ff088a28f2ed5221483ae36eb64be288
Munna Ahmed <munnaahmed2025@gmail.com>
2023-04-17 22:08:28 +0600

    commit

---
+++ src/assets/icons/index.js
@@ -0,0 +1,5 @@
+import javascript from "./javascript.svg";
+import css3 from "./css3.svg";
+import html5 from "./html5.svg";
+
+export { javascript, css3, html5 };
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,10 +6,17 @@ import { SiGmail, SiLinkedin } from "react-icons/si";
 import { BsGithub } from "react-icons/bs";
 import { FaLinkedin } from "react-icons/fa";
 import Ripple from "./button/Ripple";
+import { css3, html5, javascript } from "../assets/icons";
 // to-[#77a1fd]  from-[#4b52e4]
 
 const Intro = () => {
   const icons = [SiGmail, SiLinkedin, BsGithub];
+  const skills = [
+    { icon: javascript, title: "JavaScript" },
+    { icon: css3, title: "CSS3" },
+    { icon: html5, title: "HTML5" },
+  ];
+
   return (
     <section
       id="intro"
@@ -80,10 +87,7 @@ const Intro = () => {
         </div>
       </div> */}
       <div className="max-w-5xl w-full  mx-auto px-20">
-        <div
-          className=" 
-          flex items-center justify-start gap-5 "
-        >
+        <div className="flex items-center justify-start gap-5">
           <div className="rounded-full  sm:w-28 sm:h-28 w-24 h-24 object-cover  flex items-center duration-[400ms] transition-all  border-[#ffffff]   p-1">
             <span className="bg-gradient-to-t to-[#9b77fd]  from-[#455cc4]  w-full h-full inline-block rounded-full transition-all">
               <img
@@ -128,67 +132,25 @@ const Intro = () => {
             </div>
           </div>
         </div>
-        <p className="text-white my-14 tracking-wider">
+        <p className="text-white my-14 tracking-wider max-w-xl">
           Hi, I'm a React Developer, self-taught with experience in web
           development, designing and creating responsive and user-friendly
           websites.
         </p>
         <div>
           <p className="text-white text-xl  uppercase">Skills</p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width="48"
-            height="48"
-            viewBox="0 0 48 48"
-          >
-            <path fill="#ffd600" d="M6,42V6h36v36H6z"></path>
-            <path
-              fill="#000001"
-              d="M29.538 32.947c.692 1.124 1.444 2.201 3.037 2.201 1.338 0 2.04-.665 2.04-1.585 0-1.101-.726-1.492-2.198-2.133l-.807-.344c-2.329-.988-3.878-2.226-3.878-4.841 0-2.41 1.845-4.244 4.728-4.244 2.053 0 3.528.711 4.592 2.573l-2.514 1.607c-.553-.988-1.151-1.377-2.078-1.377-.946 0-1.545.597-1.545 1.377 0 .964.6 1.354 1.985 1.951l.807.344C36.452 29.645 38 30.839 38 33.523 38 36.415 35.716 38 32.65 38c-2.999 0-4.702-1.505-5.65-3.368L29.538 32.947zM17.952 33.029c.506.906 1.275 1.603 2.381 1.603 1.058 0 1.667-.418 1.667-2.043V22h3.333v11.101c0 3.367-1.953 4.899-4.805 4.899-2.577 0-4.437-1.746-5.195-3.368L17.952 33.029z"
-            ></path>
-          </svg>
-        </div>
-
-        {/* <div className="text-center">
-          <div className=" text-xl flex items-center justify-center pb-1">
-            <p className="text-white font-semibold">Hi, I'm Munna Ahmed</p>
-            &nbsp;
-            <HiCheckBadge className="text-blue-500 " />
+          <div>
+            {skills.map((skill) => (
+              <div
+                className="inline-block border mt-5 border-slate-500 border-collapse w-40"
+                key={skill.title}
+              >
+                <img src={skill.icon} className="w-10 h-10 inline-block p-1" />
+                <span className="text-white">{skill.title}</span>
+              </div>
+            ))}
           </div>
-
-          <span className="text-gray-200 w-full max-w-xl mx-auto  xs:text-sm text-[13px] leading-5 block tracking-[1px] pb-4 px-5">
-            I'm a &nbsp;
-            <span className="xs:w-[6rem] w-[5.8rem] inline-block text-center ">
-              <Typewriter
-                options={{ autoStart: true, loop: true, delay: 100 }}
-                onInit={(typewriter) => {
-                  typewriter
-                    .typeString("<span class='typewriter '>MERN Stack<span/>")
-                    .pauseFor(2000)
-                    .deleteAll()
-                    .typeString("<span class='typewriter'>Front-End<span/>")
-                    .pauseFor(2000)
-                    .deleteAll()
-                    .typeString("<span class='typewriter '>React<span/>")
-                    .pauseFor(2000)
-                    .deleteAll()
-                    .start();
-                }}
-              />
-            </span>
-            &nbsp; Developer from Bangladesh. I'm self-taught with experience in
-            web development, designing and creating responsive and user-friendly
-            websites.
-          </span>
-
-          <a href="https://drive.google.com/u/0/uc?id=1RhVvtP6IrrtuftlTtwQZAvgmQVRizMcy&export=download">
-            <Ripple className="text-white opacity-90 hover:opacity-100 transition-all border-white border py-2 px-3 rounded-sm">
-              Download Resume
-            </Ripple>
-          </a>
-        </div> */}
+        </div>
       </div>
     </section>
   );
