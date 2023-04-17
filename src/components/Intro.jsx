299a0b2dad1256e2549f11088649272e3e4898a0
Munna Ahmed <munnaahmed2025@gmail.com>
2023-04-17 18:18:14 +0600

    commit

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -79,12 +79,12 @@ const Intro = () => {
           </a>
         </div>
       </div> */}
-      <div className="max-w-7xl w-full  mx-auto px-20">
+      <div className="max-w-5xl w-full  mx-auto px-20">
         <div
           className=" 
           flex items-center justify-start gap-5 "
         >
-          <div className="rounded-full  w-28 h-28 object-cover  flex items-center duration-[400ms] transition-all  border-[#ffffff]   p-1">
+          <div className="rounded-full  sm:w-28 sm:h-28 w-24 h-24 object-cover  flex items-center duration-[400ms] transition-all  border-[#ffffff]   p-1">
             <span className="bg-gradient-to-t to-[#9b77fd]  from-[#455cc4]  w-full h-full inline-block rounded-full transition-all">
               <img
                 className="mx-auto w-full h-full object-cover rounded-full"
@@ -94,8 +94,8 @@ const Intro = () => {
             </span>
           </div>
           <div>
-            <div className=" text-2xl flex items-center justify-start ">
-              <p className="text-white inline-block">Hi, I'm Munna Ahmed</p>
+            <div className=" sm:text-2xl text-xl flex items-center justify-start ">
+              <p className="text-white inline-block">Munna Ahmed</p>
               &nbsp;
               <HiCheckBadge className="text-blue-500" />
             </div>
@@ -128,8 +128,28 @@ const Intro = () => {
             </div>
           </div>
         </div>
-
-        <div></div>
+        <p className="text-white my-14 tracking-wider">
+          Hi, I'm a React Developer, self-taught with experience in web
+          development, designing and creating responsive and user-friendly
+          websites.
+        </p>
+        <div>
+          <p className="text-white text-xl  uppercase">Skills</p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="48"
+            height="48"
+            viewBox="0 0 48 48"
+          >
+            <path fill="#ffd600" d="M6,42V6h36v36H6z"></path>
+            <path
+              fill="#000001"
+              d="M29.538 32.947c.692 1.124 1.444 2.201 3.037 2.201 1.338 0 2.04-.665 2.04-1.585 0-1.101-.726-1.492-2.198-2.133l-.807-.344c-2.329-.988-3.878-2.226-3.878-4.841 0-2.41 1.845-4.244 4.728-4.244 2.053 0 3.528.711 4.592 2.573l-2.514 1.607c-.553-.988-1.151-1.377-2.078-1.377-.946 0-1.545.597-1.545 1.377 0 .964.6 1.354 1.985 1.951l.807.344C36.452 29.645 38 30.839 38 33.523 38 36.415 35.716 38 32.65 38c-2.999 0-4.702-1.505-5.65-3.368L29.538 32.947zM17.952 33.029c.506.906 1.275 1.603 2.381 1.603 1.058 0 1.667-.418 1.667-2.043V22h3.333v11.101c0 3.367-1.953 4.899-4.805 4.899-2.577 0-4.437-1.746-5.195-3.368L17.952 33.029z"
+            ></path>
+          </svg>
+        </div>
 
         {/* <div className="text-center">
           <div className=" text-xl flex items-center justify-center pb-1">
